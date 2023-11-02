47ce2bd7fa8a5f96185dbfd17db0ad9da8441d33
Caetano Costa <caetanogonca@gmail.com>
2023-11-02 19:27:13 -0300

    Terminei as validacoes

--- pages/scriptCadastro.js
+++ pages/scriptCadastro.js
@@ -2,7 +2,6 @@ nomeCadastro = document.getElementById("nomeCadastro")
 invalid = document.getElementById("erroNomeCadastro")
 MensagemNomeCadastro = document.getElementById("MensagemNomeCadastro")
 
-// validacao da senha = valor.length >= 8 && valor.length <= 20 && /[0-9]/.test(valor) && /[a-z]/.test(valor) && /[A-Z]/.test(valor)
 
 nomeCadastro.addEventListener("keyup", () => {
     const valor = nomeCadastro.value;
@@ -42,6 +41,7 @@ sobrenomeCadastro.addEventListener("keyup", () => {
     }
 })
 
+
 usuarioCadastro = document.getElementById("usuarioCadastro")
 erroUsuarioCadastro = document.getElementById("erroUsuarioCadastro")
 MensagemUsuarioCadastro = document.getElementById("MensagemUsuarioCadastro")
@@ -60,5 +60,76 @@ usuarioCadastro.addEventListener("keyup", () => {
         erroUsuarioCadastro.classList.remove("valid-feedback")
         erroUsuarioCadastro.classList.add("invalid-feedback")
         MensagemUsuarioCadastro.innerText = "Usuário inválido"
+    }
+})
+
+
+emailCadastro = document.getElementById("emailCadastro")
+erroEmailCadastro = document.getElementById("erroEmailCadastro")
+MensagemEmailCadastro = document.getElementById("MensagemEmailCadastro")
+
+
+emailCadastro.addEventListener("keyup", () => {
+    const valor = emailCadastro.value;
+    const isValid = validateEmail(valor);
+    
+    if(isValid){
+        erroEmailCadastro.classList.remove("invalid-feedback")
+        erroEmailCadastro.classList.add("valid-feedback")
+        MensagemEmailCadastro.innerText = "Email válido"  
+    }else{
+        erroEmailCadastro.style.display="block"
+        erroEmailCadastro.classList.remove("valid-feedback")
+        erroEmailCadastro.classList.add("invalid-feedback")
+        MensagemEmailCadastro.innerText = "Email inválido"
+    }
+})
+
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+
+
+senhaCadastro = document.getElementById("senhaCadastro")
+erroSenhaCadastro = document.getElementById("erroSenhaCadastro")
+MensagemSenhaCadastro = document.getElementById("MensagemSenhaCadastro")
+
+
+senhaCadastro.addEventListener("keyup", () => {
+    const valor = senhaCadastro.value;
+    const isValid = valor.length >= 8 && valor.length <= 20 && /[0-9]/.test(valor) && /[a-z]/.test(valor) && /[A-Z]/.test(valor) && /[!|@|#|$|%|^|&|*|(|)|-|_]/.test(valor);
+    
+    if(isValid){
+        erroSenhaCadastro.classList.remove("invalid-feedback")
+        erroSenhaCadastro.classList.add("valid-feedback")
+        MensagemSenhaCadastro.innerText = "Senha válida"  
+    }else{
+        erroSenhaCadastro.style.display="block"
+        erroSenhaCadastro.classList.remove("valid-feedback")
+        erroSenhaCadastro.classList.add("invalid-feedback")
+        MensagemSenhaCadastro.innerText = "Senha inválida"
+    }
+})
+
+
+confirmarCadastro = document.getElementById("confirmarCadastro")
+erroConfirmarCadastro = document.getElementById("erroConfirmarCadastro")
+MensagemConfirmarCadastro = document.getElementById("MensagemConfirmarCadastro")
+
+
+confirmarCadastro.addEventListener("keyup", () => {
+    const valor = confirmarCadastro.value;
+    const isValid = valor===senhaCadastro.value && valor.length >= 8 && valor.length <= 20 && /[0-9]/.test(valor) && /[a-z]/.test(valor) && /[A-Z]/.test(valor) && /[!|@|#|$|%|^|&|*|(|)|-|_]/.test(valor);
+    
+    if(isValid){
+        erroConfirmarCadastro.classList.remove("invalid-feedback")
+        erroConfirmarCadastro.classList.add("valid-feedback")
+        MensagemConfirmarCadastro.innerText = "Senha válida"  
+    }else{
+        erroConfirmarCadastro.style.display="block"
+        erroConfirmarCadastro.classList.remove("valid-feedback")
+        erroConfirmarCadastro.classList.add("invalid-feedback")
+        MensagemConfirmarCadastro.innerText = "Senha inválida"
     }
 })
