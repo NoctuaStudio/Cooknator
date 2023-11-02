ebb9a9f5a09d72bc5673b90a4bf53c88156432bd
Caetano Costa <caetanogonca@gmail.com>
2023-11-02 19:00:55 -0300

    Validacao de Usuario feita

--- pages/scriptCadastro.js
+++ pages/scriptCadastro.js
@@ -2,22 +2,21 @@ nomeCadastro = document.getElementById("nomeCadastro")
 invalid = document.getElementById("erroNomeCadastro")
 MensagemNomeCadastro = document.getElementById("MensagemNomeCadastro")
 
+// validacao da senha = valor.length >= 8 && valor.length <= 20 && /[0-9]/.test(valor) && /[a-z]/.test(valor) && /[A-Z]/.test(valor)
 
 nomeCadastro.addEventListener("keyup", () => {
     const valor = nomeCadastro.value;
-    const isValid = valor.length >= 8 && valor.length <= 20 && /[0-9]/.test(valor) && /[a-z]/.test(valor) && /[A-Z]/.test(valor);
+    const isValid = valor.length >= 3 && valor.length <= 20;
     
     if(isValid){
         invalid.classList.remove("invalid-feedback")
         invalid.classList.add("valid-feedback")
         MensagemNomeCadastro.innerText = "Nome válido"
-        
     }else{
         invalid.style.display="block"
         invalid.classList.remove("valid-feedback")
         invalid.classList.add("invalid-feedback")
         MensagemNomeCadastro.innerText = "Nome inválido"
-       
     }
 })
 
@@ -34,13 +33,32 @@ sobrenomeCadastro.addEventListener("keyup", () => {
     if(isValid){
         erroSobrenomeCadastro.classList.remove("invalid-feedback")
         erroSobrenomeCadastro.classList.add("valid-feedback")
-        MensagemSobrenomeCadastro.innerText = "Sobrenome válido"
-        
+        MensagemSobrenomeCadastro.innerText = "Sobrenome válido"  
     }else{
         erroSobrenomeCadastro.style.display="block"
         erroSobrenomeCadastro.classList.remove("valid-feedback")
         erroSobrenomeCadastro.classList.add("invalid-feedback")
         MensagemSobrenomeCadastro.innerText = "Sobrenome inválido"
-       
+    }
+})
+
+usuarioCadastro = document.getElementById("usuarioCadastro")
+erroUsuarioCadastro = document.getElementById("erroUsuarioCadastro")
+MensagemUsuarioCadastro = document.getElementById("MensagemUsuarioCadastro")
+
+
+usuarioCadastro.addEventListener("keyup", () => {
+    const valor = usuarioCadastro.value;
+    const isValid = valor.length >= 3 && valor.length <= 20;
+    
+    if(isValid){
+        erroUsuarioCadastro.classList.remove("invalid-feedback")
+        erroUsuarioCadastro.classList.add("valid-feedback")
+        MensagemUsuarioCadastro.innerText = "Usuário válido"  
+    }else{
+        erroUsuarioCadastro.style.display="block"
+        erroUsuarioCadastro.classList.remove("valid-feedback")
+        erroUsuarioCadastro.classList.add("invalid-feedback")
+        MensagemUsuarioCadastro.innerText = "Usuário inválido"
     }
 })
