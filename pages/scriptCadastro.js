1df4a653124adb761399c934f132fa1f3389485f
Gustavo Henrique de Moraes <gustavomraes104@gmail.com>
2023-11-02 18:25:23 -0300

    Mexendo no Script Cadastro

--- pages/scriptCadastro.js
+++ pages/scriptCadastro.js
@@ -1,22 +1,8 @@
-
-var form = document.querySelector('form')
-
-form.addEventListener('submit', function (event) {
-    if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-    }
-
-    form.classList.add('was-validated')
-})
-
-
 nomeCadastro = document.getElementById("nomeCadastro")
 invalid = document.getElementById("erroNomeCadastro")
 
 nomeCadastro.addEventListener("keyup", () => {
     const valor = nomeCadastro.value;
-    //OBS: Essa validação é para senha
     const isValid = valor.length >= 8 && valor.length <= 20 && /[0-9]/.test(valor) && /[a-z]/.test(valor) && /[A-Z]/.test(valor);
     
     if(isValid){
@@ -24,6 +10,7 @@ nomeCadastro.addEventListener("keyup", () => {
         invalid.classList.add("valid-feedback")
         
     }else{
+        invalid.style.display="block"
         invalid.classList.remove("valid-feedback")
         invalid.classList.add("invalid-feedback")
        
