22427c96c0f317504884977f961f224f94d2ea57
Gustavo Henrique de Moraes <gustavomraes104@gmail.com>
2023-11-07 15:39:25 -0300

    Utilizando o SQL 2 e update Query

--- Script Create DB.sql
+++ Script Create DB.sql
@@ -1,20 +1,55 @@
-DROP DATABASE CookNator;
+DROP DATABASE IF EXISTS CookNator;
 CREATE DATABASE CookNator;
 USE CookNator;
 
-CREATE TABLE Usuarios (
-    ID INT PRIMARY KEY,
-    Nome VARCHAR(50),
-    Sobrenome VARCHAR(50),
-    Cidade VARCHAR(50)
+CREATE TABLE Ingredientes(
+ID INT PRIMARY KEY ,
+Nome VARCHAR (50) 
 );
 
-INSERT INTO Usuarios (ID, Nome, Sobrenome, Cidade) VALUES
-(1, 'João', 'Silva', 'São Paulo'),
-(2, 'Maria', 'Santos', 'Rio de Janeiro'),
-(3, 'Pedro', 'Costa', 'Belo Horizonte'),
-(4, 'Ana', 'Oliveira', 'Salvador'),
-(5, 'Carlos', 'Rocha', 'Recife');
+CREATE TABLE Usuarios(
+ID INT PRIMARY KEY,
+Nome VARCHAR(50),
+Senha VARCHAR(50),
+Email VARCHAR(50),
+Username VARCHAR(30),
+Sobrenome VARCHAR(30)
+);
 
-SELECT * FROM Usuarios;
 
+
+CREATE TABLE Tipo_Receita(
+ID INT PRIMARY KEY,
+Tipo VARCHAR(50)
+);
+
+CREATE TABLE Receitas(
+ID INT PRIMARY KEY ,
+Nome VARCHAR(80),
+Descricao VARCHAR(255),
+ID_Tipo INT,
+Imagem VARCHAR(255),
+Avaliacao FLOAT,
+ID_Usuario INT,
+Tamanho ENUM ('Pequeno', 'Médio', 'Grande'),
+Dieta ENUM ('Carnívoro', 'Vegetariano', 'Vegano'),
+FOREIGN KEY (ID_Tipo) REFERENCES Tipo_Receita(ID),
+FOREIGN KEY (ID_Usuario) REFERENCES Usuarios(ID)
+);
+
+CREATE TABLE Ingredientes_Da_Receita(
+ID INT PRIMARY KEY,
+ID_Receita INT,
+ID_Ingrediente INT,
+Quantidade varchar(50),
+FOREIGN KEY (ID_Receita) REFERENCES Receitas(ID),
+FOREIGN KEY (ID_Ingrediente) REFERENCES Ingredientes(ID)
+);
+
+CREATE TABLE Receitas_Salvas(
+ID INT PRIMARY KEY,
+ID_Receita INT,
+ID_Usuario INT,
+FOREIGN KEY (ID_Receita) REFERENCES Tipo_Receita(ID),
+FOREIGN KEY (ID_Usuario) REFERENCES Usuarios(ID)
+);
--- db.js
+++ db.js
@@ -1,8 +1,8 @@
-import mysql from "mysql"
+import mysql from "mysql2"
  
 export const db = mysql.createConnection({
     host:"localhost",
-    user:"root",
+    user:"estudante1",
     password:"",
     database:"CookNator",
 })
