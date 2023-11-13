272cc0fe86703a17d293d49efc234a694f498b82
Gustavo Lemos <galaxywander0@gmail.com>
2023-11-13 18:50:42 -0300

    Criando o modal para cadastrar as receitas!

--- frontend/pages/ScriptIndex.js
+++ frontend/pages/ScriptIndex.js
@@ -9,4 +9,26 @@ botaoLogin.addEventListener('click', function (event) {
     window.location.href = "singin-up.html"
 })
 
-//Nao faço ideia de como faz isso, peguei no Google e não foi. Por favor ajudar.
+//Nao faço ideia de como faz isso, peguei no Google e não foi. Por favor ajudar. KKKKKKKKK eu n sou bom de backend, vim fazer o modal aparecer
+
+var botaoCriarReceita = document.getElementById("adcionarReceita");
+var modal = document.getElementById("modalReceita");
+var botaoFechar = document.getElementById("fecharModal");
+var body = document.getElementsByTagName("body")[0];
+var fundoEscuro = document.getElementById("fundoEscuro");
+
+botaoCriarReceita.addEventListener('click', function (event) {
+    modal.classList.add("d-block");
+    body.style.overflow = "hidden";
+    fundoEscuro.classList.remove("d-none");
+    fundoEscuro.classList.add("d-block");
+
+})
+
+botaoFechar.addEventListener('click', function (event) {
+    modal.classList.remove("d-block");
+    body.style.overflow = "auto";
+    fundoEscuro.classList.remove("d-block");
+    fundoEscuro.classList.add("d-none");
+})
+
