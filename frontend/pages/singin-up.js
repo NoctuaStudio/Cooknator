e485bbedd49fa516144c22ca6f0a5be7c29f95c5
Gustavo Lemos <galaxywander0@gmail.com>
2023-11-14 23:00:27 -0300

    dei uma ajeitada no que ele q ueria

--- frontend/pages/singin-up.js
+++ frontend/pages/singin-up.js
@@ -1,107 +1,107 @@
 const loginCard = document.getElementById('login-card');
-      const cadastroCard = document.getElementById('cadastro-card');
-      const buttonLogin = document.getElementById('login-button');
-      const buttonCadastro = document.getElementById('cadastro-button');
-      const title = document.getElementsByTagName('title')[0];
-      const textocadastro = document.getElementById('texto-cadastro');
-      const textologin = document.getElementById('texto-login');
-    
-      function showLoginCard() {
-        cadastroCard.classList.add('rotate-cross');
-        setTimeout(() => {
-          loginCard.style.display = 'block';
-          cadastroCard.style.display = 'none';
-          setTimeout(() => {
-            cadastroCard.classList.remove('rotate-cross');
-            //buttonCadastro.disabled = "false";  //queria fazer isso funcionar para não bugar quando clica muitas vezes, porem não consegui	
-          }, 1500); 
-        }, 750);
-        //buttonLogin.disabled = "true";
-        textocadastro.style.opacity = 0;
-        textologin.style.opacity = 1;
-        title.textContent = 'Login';
-        buttonLogin.classList.remove('inactive');
-        buttonCadastro.classList.add('inactive');
-        buttonCadastro.classList.remove('active');
-        buttonLogin.classList.add('active');
-      }
-    
-      function showCadastroCard() {
-        loginCard.classList.add('rotate-cross');
-        setTimeout(() => {
-          cadastroCard.style.display = 'block';
-          loginCard.style.display = 'none';
-          setTimeout(() => {
-            loginCard.classList.remove('rotate-cross');
-           // buttonLogin.disebled = "false";    
-          }, 1500);
-        }, 750);
-        //buttonCadastro.disebled = "true";
-        textologin.style.opacity = 0;
-        textocadastro.style.opacity = 1;
-        title.textContent = 'Cadastro';
-        buttonLogin.classList.add('inactive');
-        buttonLogin.classList.remove('active');
-        buttonCadastro.classList.add('active');
-        buttonCadastro.classList.remove('inactive');
-      }
+const cadastroCard = document.getElementById('cadastro-card');
+const buttonLogin = document.getElementById('login-button');
+const buttonCadastro = document.getElementById('cadastro-button');
+const title = document.getElementsByTagName('title')[0];
+const textocadastro = document.getElementById('texto-cadastro');
+const textologin = document.getElementById('texto-login');
 
-      loginCard.addEventListener('animationend', () => {
-        loginCard.classList.remove('rotate-login'); // Remove a classe de rotação do cartão de login
-      });
-    
-      cadastroCard.addEventListener('animationend', () => {
-        cadastroCard.classList.remove('rotate-cadastro'); // Remove a classe de rotação do cartão de cadastro
-      });
+function showLoginCard() {
+cadastroCard.classList.add('rotate-cross');
+setTimeout(() => {
+    loginCard.style.display = 'block';
+    cadastroCard.style.display = 'none';
+    setTimeout(() => {
+    cadastroCard.classList.remove('rotate-cross');
+    //buttonCadastro.disabled = "false";  //queria fazer isso funcionar para não bugar quando clica muitas vezes, porem não consegui	
+    }, 1500); 
+}, 750);
+//buttonLogin.disabled = "true";
+textocadastro.style.opacity = 0;
+textologin.style.opacity = 1;
+title.textContent = 'Login';
+buttonLogin.classList.remove('inactive');
+buttonCadastro.classList.add('inactive');
+buttonCadastro.classList.remove('active');
+buttonLogin.classList.add('active');
+}
 
-      buttonLogin.addEventListener('click', showLoginCard);
-      buttonCadastro.addEventListener('click', showCadastroCard);
+function showCadastroCard() {
+loginCard.classList.add('rotate-cross');
+setTimeout(() => {
+    cadastroCard.style.display = 'block';
+    loginCard.style.display = 'none';
+    setTimeout(() => {
+    loginCard.classList.remove('rotate-cross');
+    // buttonLogin.disebled = "false";    
+    }, 1500);
+}, 750);
+//buttonCadastro.disebled = "true";
+textologin.style.opacity = 0;
+textocadastro.style.opacity = 1;
+title.textContent = 'Cadastro';
+buttonLogin.classList.add('inactive');
+buttonLogin.classList.remove('active');
+buttonCadastro.classList.add('active');
+buttonCadastro.classList.remove('inactive');
+}
 
-      function mostrarOcultarConfirmarSenha() {
-            var senhaConfirmarInput = document.getElementById("confirmarCadastro");
-            var icon1 = document.getElementById("eye-dismissed2");
-            var icon2 = document.getElementById("eye-vissible2");
+loginCard.addEventListener('animationend', () => {
+loginCard.classList.remove('rotate-login'); // Remove a classe de rotação do cartão de login
+});
 
-            if (senhaConfirmarInput.type === "password") {
-                icon1.style.display = "none";
-                icon2.style.display = "block";
-                senhaConfirmarInput.type = "text";
-            } else if (senhaConfirmarInput.type === "text"){
-                icon1.style.display = "block";
-                icon2.style.display = "none";
-                senhaConfirmarInput.type = "password";
-            }
+cadastroCard.addEventListener('animationend', () => {
+cadastroCard.classList.remove('rotate-cadastro'); // Remove a classe de rotação do cartão de cadastro
+});
 
-        }    
-        function mostrarOcultarSenha() {
-            var senhaInput = document.getElementById("senhaCadastro");
-            var icon1 = document.getElementById("eye-dismissed1");
-            var icon2 = document.getElementById("eye-vissible1");
-            if (senhaInput.type === "password") {
-                icon1.style.display = "none";
-                icon2.style.display = "block";
-                senhaInput.type = "text";
-            } else if (senhaInput.type === "text") {
-                icon1.style.display = "block";
-                icon2.style.display = "none";
-                senhaInput.type = "password";
-            }
-        }
-        function mostrarOcultarSenhaLogin() {
-            var senhaInput = document.getElementById("senhaLogin");
-            var icon1 = document.getElementById("eye-dismissed3");
-            var icon2 = document.getElementById("eye-vissible3");
-            if (senhaInput.type === "password") {
-                icon1.style.display = "none";
-                icon2.style.display = "block";
-                senhaInput.type = "text";
-            } else if (senhaInput.type === "text") {
-                icon1.style.display = "block";
-                icon2.style.display = "none";
-                senhaInput.type = "password";
-            }
-        }
+buttonLogin.addEventListener('click', showLoginCard);
+buttonCadastro.addEventListener('click', showCadastroCard);
 
+function mostrarOcultarConfirmarSenha() {
+    var senhaConfirmarInput = document.getElementById("confirmarCadastro");
+    var icon1 = document.getElementById("eye-dismissed2");
+    var icon2 = document.getElementById("eye-vissible2");
+
+    if (senhaConfirmarInput.type === "password") {
+        icon1.style.display = "none";
+        icon2.style.display = "block";
+        senhaConfirmarInput.type = "text";
+    } else if (senhaConfirmarInput.type === "text"){
+        icon1.style.display = "block";
+        icon2.style.display = "none";
+        senhaConfirmarInput.type = "password";
+    }
+
+} 
+   
+function mostrarOcultarSenha() {
+    var senhaInput = document.getElementById("senhaCadastro");
+    var icon1 = document.getElementById("eye-dismissed1");
+    var icon2 = document.getElementById("eye-vissible1");
+    if (senhaInput.type === "password") {
+        icon1.style.display = "none";
+        icon2.style.display = "block";
+        senhaInput.type = "text";
+    } else if (senhaInput.type === "text") {
+        icon1.style.display = "block";
+        icon2.style.display = "none";
+        senhaInput.type = "password";
+    }
+}
+function mostrarOcultarSenhaLogin() {
+    var senhaInput = document.getElementById("senhaLogin");
+    var icon1 = document.getElementById("eye-dismissed3");
+    var icon2 = document.getElementById("eye-vissible3");
+    if (senhaInput.type === "password") {
+        icon1.style.display = "none";
+        icon2.style.display = "block";
+        senhaInput.type = "text";
+    } else if (senhaInput.type === "text") {
+        icon1.style.display = "block";
+        icon2.style.display = "none";
+        senhaInput.type = "password";
+    }
+}
 
 let validaçõesLogin = [false, false]
 
