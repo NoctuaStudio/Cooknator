288fc6c8e4d05b0be822a03e178dad83999556ad
Gustavo Lemos <galaxywander0@gmail.com>
2023-11-20 15:40:26 -0300

    todas as paginas redirecionam para o sing-up

---
+++ frontend/scripts/redirecionarSing-Up.js
@@ -0,0 +1,6 @@
+const botaoCadastroLogin = document.getElementById("loginCadastro")
+
+botaoCadastroLogin.addEventListener('submit', function (event) {
+    event.preventDefault()
+    location.href = "singin-up.html"
+})
