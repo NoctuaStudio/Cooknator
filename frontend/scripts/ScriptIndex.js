0a2fac1a8df319e0a49ea1a1316d5cb92215d391
Gustavo Henrique de Moraes <gustavomraes104@gmail.com>
2023-11-21 19:25:06 -0300

    Agora o index está apresentando receitas

--- frontend/scripts/ScriptIndex.js
+++ frontend/scripts/ScriptIndex.js
@@ -5,6 +5,59 @@ botaoCadastroLogin.addEventListener('submit', function (event) {
     location.href = "singin-up.html"
 })
 
+// FAZENDO APARECER AS RECEITAS:
+
+  
+    fetch("http://localhost:8010/receita", {
+            method: "GET",          
+            headers:{
+                "Content-Type": "application/json"
+            }}) 
+        .then(resposta => resposta.json())
+        .then(receitas => {
+            console.log("COLOCANDO RECEITAS PADRAO")
+            console.log(" RECEITAS DO PREENCHER RECEITAS:" + receitas)
+    const LocalReceitasDiv = document.getElementById("ApresentarReceitas")
+
+    
+    for (let i = 0; i < 3; i++) {
+        const div = document.createElement('div');
+        const card = document.createElement('div');
+        const img = document.createElement('img');
+        const cardBody = document.createElement('div');
+        const h4 = document.createElement('h4');
+        const p = document.createElement('p');
+        div.classList.add('col-md-4');
+        div.classList.add('pt-md-4');
+        card.classList.add('card');
+        img.classList.add('img-fluid');
+        img.classList.add('tamanho-imagem')
+        cardBody.classList.add('card-body');
+        h4.classList.add('card-title');
+        p.classList.add('card-text');
+        img.src = receitas[i].Imagem; 
+        h4.textContent = receitas[i].Nome
+        p.textContent = receitas[i].Descricao
+        div.appendChild(card);
+        card.appendChild(img);
+        card.appendChild(cardBody);
+        cardBody.appendChild(h4);
+        cardBody.appendChild(p);
+        LocalReceitasDiv.appendChild(div);   
+        
+        div.addEventListener('click', function (event) {
+            const receitaId = receitas[i].ID; // Substitua isso com o ID ou identificador da receita
+            location.href = `receita.html?id=${receitaId}`;
+        })}
+    })
+    
+
+
+
+
+
+
+
 //Nao faço ideia de como faz isso, peguei no Google e não foi. Por favor ajudar. KKKKKKKKK eu n sou bom de backend, vim fazer o modal aparecer
 
 var botaoCriarReceita = document.getElementById("adcionarReceita");
